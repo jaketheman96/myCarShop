f51735c0519e4b7653b0b618a74c61a484f2fb0d
Jake Chien <jake_chien@outlook.com>
2022-11-17 17:47:43 -0300

    feat: implement carODM

---
+++ src/Models/carODM.ts
@@ -0,0 +1,31 @@
+import {
+  Model,
+  Schema,
+  model,
+  models,
+} from 'mongoose';
+import Icar from '../Interfaces/car.interface';
+
+class CarODM {
+  private schema: Schema;
+  private model: Model<Icar>;
+
+  constructor() {
+    this.schema = new Schema<Icar>({
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean, required: true },
+      buyValue: { type: Number, required: true },
+      doorsQty: { type: Number, required: true },
+      seatsQty: { type: Number, required: true },
+    });
+    this.model = models.Cars || model('Cars', this.schema);
+  }
+
+  public async create(car: Icar): Promise<Icar> {
+    return this.model.create({ ...car });
+  }
+}
+
+export default CarODM;
